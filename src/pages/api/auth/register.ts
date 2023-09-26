e0f2edee8f23bff5296b34ee6667be694f8c0e36
Romina Fabi <rominafabi@air-di-romina.homenet.telecomitalia.it>
2023-09-26 03:36:10 +0200

    ✅ Feat: Created style for register page

--- src/pages/api/auth/register.ts
+++ src/pages/api/auth/register.ts
@@ -6,16 +6,15 @@ export const POST: APIRoute = async ({ request, redirect }) => {
   const auth = getAuth(app);
 
   /* Get form data */
-  const formData = await request.formData();
-  const email = formData.get("email")?.toString();
-  const password = formData.get("password")?.toString();
-  const name = formData.get("name")?.toString();
+  const requestBody = await request.text();
+  const res = JSON.parse(requestBody);
+  const email = res.data.email;
+  const password = res.data.password;
 
-  if (!email || !password || !name) {
-    return new Response(
-      "Missing form data",
-      { status: 400 }
-    );
+  if (!email || !password) {
+    return new Response(JSON.stringify({
+      error: "Missing form data"
+    }));
   }
 
   /* Create user */
@@ -23,14 +22,14 @@ export const POST: APIRoute = async ({ request, redirect }) => {
     await auth.createUser({
       email,
       password,
-      displayName: name,
     });
+    return new Response(JSON.stringify({
+      message: "User Created!",
+    }));
   } catch (error: any) {
-   console.log(error);
-    return new Response(
-      "Something went wrong",
-      { status: 400 }
-    );
+    console.log("error is:", error)
+    return new Response(JSON.stringify({
+      error: "Something went wrong : " + error.errorInfo.message
+    }));
   }
-  return redirect("/signin");
 };
