1b11137e8c9673add075f3369a640bc3a0edb206
Romina Fabi <rominafabi@MacBook-Air-di-Romina.local>
2023-10-05 02:59:38 +0200

    ✅ Feat: Created social bar and animations

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,9 +8,18 @@ module.exports = {
 			'white':'#ffffff',
 			'not-activate': '#d4d4d4e6',
 			'border-color': '#262626',
-			'form-not-activate': '#c4c4c4'
+			'form-not-activate': '#c4c4c4',
+			'gold':'#d5b33d'
 		},
-		extend: {},
+		extend: {
+			backgroundImage: {
+				'hero-pattern': "url('/src/images/libreria_2.png')",
+				'black-pattern': "url('/src/images/green.png')"
+			 },
+			 inset: {
+				'center-socials': 'calc(50% - 81px)',
+			 }
+		},
 	},
 	plugins: [],
 }
